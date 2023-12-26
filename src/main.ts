d3275666f8c6d715d0dc5a5d14e28930d8cc602e
Bruce <149814380+brucehamada@users.noreply.github.com>
2023-12-26 23:44:32 +0900

    ss

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//aaa
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
